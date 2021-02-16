a70790d76c09277c4a5f52b7cc01b4dc1e0f6323
LED <transhumanism00@mail.ru>
2021-02-16 13:54:35 +0300

    Update function_grad_descent.py

--- function_grad_descent.py
+++ function_grad_descent.py
@@ -23,3 +23,16 @@ for _ in range(500):
     w.grad.zero_()# something is missing here!
 
 print(w) 
+
+# Перепишите пример, используя torch.optim.SGD
+
+w = torch.tensor([[5., 10.], [1., 2.]], requires_grad=True)
+alpha = 0.001
+optimizer = torch.optim.SGD([w], alpha) # put our code here. Do not forget [] inside SGD constructor !!!!
+
+for _ in range(500):
+    # it's critical to calculate function inside the loop:
+    function = (w + 7).log().log().prod()
+    function.backward()
+    optimizer.step()
+    optimizer.zero_grad()
